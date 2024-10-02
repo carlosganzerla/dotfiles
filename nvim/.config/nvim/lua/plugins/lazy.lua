dffefce506ccd66ed48b6cb27ff07e92b07be334
carlosganzerla <carlosganzrl@hotmail.com>
2024-10-02 19:09:22 -0300

    Migrated old snippets, fixed some keybindings

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -65,11 +65,37 @@ require("lazy").setup({
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
                     },
+                    ['<C-k>'] = cmp.mapping(function (fallback)
+                        if luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-j>'] = cmp.mapping(function (fallback)
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-l>'] = cmp.mapping(function (fallback)
+                        if luasnip.choice_active() then
+                            luasnip.change_choice(1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function (fallback)
+                        if luasnip.choice_active() then
+                            luasnip.change_choice(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
                     ['<Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
                         else
                             fallback()
                         end
@@ -77,8 +103,6 @@ require("lazy").setup({
                     ['<S-Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
                         else
                             fallback()
                         end
--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -1,6 +1,4 @@
 local ls = require("luasnip")
-local types = require("luasnip.util.types")
-
 -- some shorthands...
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -10,10 +8,22 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
 
 local function copy(args)
     return args[1]
 end
+
 
 ls.setup({
     keep_roots = true,
@@ -56,7 +66,7 @@ ls.add_snippets("markdown", {
         t("# `"),
         i(1, "table"),
         t("` table"),
-        t({ "", "", "## Fields", "", "## SQL", "", "```sql", ""}),
+        t({ "", "", "## Fields", "", "## SQL", "", "```sql", "" }),
         t("CREATE TABLE "),
         f(copy, 1),
         t({ " (", ");", "```" })
@@ -65,7 +75,7 @@ ls.add_snippets("markdown", {
         t("# `"),
         i(1, "type"),
         t("` enum"),
-        t({ "", "", "An enumeration...", "", "The following values apply:", "",  "## SQL", "", "```sql", ""}),
+        t({ "", "", "An enumeration...", "", "The following values apply:", "", "## SQL", "", "```sql", "" }),
         t("CREATE TYPE "),
         f(copy, 1),
         t({ " AS ENUM (", ");", "```" })
@@ -74,8 +84,86 @@ ls.add_snippets("markdown", {
         t("# `"),
         i(1, "function"),
         t("` function"),
-        t({ "", "", "This function...", "", "",  "## Inputs", "", "", "## Output", "", "",  "```sql", "", "```"}),
+        t({ "", "", "This function...", "", "", "## Inputs", "", "", "## Output", "", "", "```sql", "", "```" }),
     }),
 })
+
+ls.add_snippets("typescript", {
+    s("eslint max lines", t("/* eslint max-lines-per-function: 0 */")),
+    s("jest test", fmt(
+        [[
+            describe('{}', () => {{
+                beforeEach(() => {{
+                }});
+
+                it('{}', {}() => {{
+                    {}
+                }});
+            }});
+        ]],
+        {
+            i(1, "Feature"),
+            i(2, "does something"),
+            c(3, { t(""), t("async ") }),
+            i(0)
+        }))
+});
+
+ls.add_snippets("all", {
+    s("story", fmt(
+        [[
+            import React from 'react';
+            import {{ StoryFn, Meta }} from '@storybook/react';
+            import {{ {}, {}Props }} from './{}';
+
+            export default {{
+                title: '{}{}',
+                component: {},
+                args: {{
+                    {}
+                }},
+            }} as Meta;
+
+            const Template: StoryFn<{}Props> = args => <{} {{...args}} />;
+
+            export const Default = Template.bind({{}});
+        ]],
+        {
+            i(1),
+            f(copy, 1),
+            f(copy, 1),
+            i(2),
+            f(copy, 1),
+            f(copy, 1),
+            i(0),
+            f(copy, 1),
+            f(copy, 1),
+        }
+    )),
+    s("visual component", fmt(
+        [[
+            import React from 'react';
+
+            export type {}Props = {{
+            }};
+
+            export const {} = ({{ }}: {}Props): JSX.Element => {{
+                return (
+                    <{}>{}</{}>
+                );
+            }};
+        ]],
+        {
+            i(1),
+            f(copy, 1),
+            f(copy, 1),
+            i(2),
+            i(0),
+            f(copy, 2),
+        }
+    )),
+
+})
+
 
 vim.keymap.set("n", "<leader><leader>s", "<cmd>luafile ~/.config/nvim/lua/plugins/snippets.lua<CR>")
