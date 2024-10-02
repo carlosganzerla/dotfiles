7ac29bd5d5980a38f37ba3876536b27096ee1a3e
carlosganzerla <carlosganzrl@hotmail.com>
2024-10-02 18:19:36 -0300

    Fix + getting rid of old stuff

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ local builtin = require('telescope.builtin')
 require('telescope').setup {
     defaults = {
         layout_strategy = "horizontal",
+        file_ignore_patterns = { ".git/" },
         layout_config = {
             preview_width = 0.4,
             horizontal = {
@@ -31,7 +32,9 @@ require('telescope').setup {
 }
 
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-p>', function()
+    builtin.find_files({ hidden = true })
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>rg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>bl', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>bd', builtin.git_bcommits, { desc = 'Telescope git commits' })
